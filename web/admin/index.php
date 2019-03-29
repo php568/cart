c7f9a039e3e4ad568bc1e8b4e35c5dca94eb58ae
liutengfei1 <liutengfei@shopex.cn>
2019-03-29 21:20:46 +0800

    练习

---
+++ web/admin/index.php
@@ -0,0 +1,34 @@
+<?php
+
+include 'init.php';
+?>
+<!doctype html>
+<html lang="en">
+<head>
+<meta charset="utf-8" />
+<link rel="shortcut icon" href="/favicon.ico" />
+<link rel="stylesheet" type="text/css" href="http://www.ulpin.com/css/bootstrap.min.css">
+<style>
+dd{word-wrap:break-word;};
+</style>
+<title>管理后台_秒杀系统</title>
+<script type="text/javascript" src="/static/js/jquery-1.7.2-min.js"></script>
+<script type="text/javascript" src="/static/js/bootstrap.min.js"></script>
+<base target="_self"/>
+</head>
+<body>
+<br/>
+<br/>
+<br/>
+<br/>
+<div class="container">
+	<center>
+		<a href="/admin/active.php" class="btn">活动管理</a><br/><br/>
+        <a href="/admin/trade.php" class="btn">订单管理</a><br/><br/>
+		<a href="/admin/goods.php" class="btn">商品管理</a><br/><br/>
+		<a href="/admin/question.php" class="btn">问答管理</a><br/><br/>
+		<a href="/admin/log.php" class="btn">日志管理</a><br/><br/>
+	</center>
+</div>
+</body>
+</html>
